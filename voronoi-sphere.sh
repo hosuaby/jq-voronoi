e66abd98dc579ad97b5cb7ed3797b75f3e7badbe
Alexei KLENIN <alexei.klenin@gmail.com>
2020-04-26 12:12:01 +0200

    Refactor scripts

--- voronoi-sphere.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Get directory of this script
-SCRIPT=`realpath -s $0`
-BASEDIR=`dirname $SCRIPT`
-
-jq -cM -L "$BASEDIR/voronoi" -f voronoi/voronoi-sphere.jq
--- voronoi.sh
+++ voronoi.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-# Wrapper to make voronoi/voronoi.jq runnable from any directory
+# Wrapper to make voronoi/voronoi.jq and voronoi/voronoi_sphere.jq runnable from any directory
 
 # Get directory of this script
 SCRIPT=`realpath -s $0`
 BASEDIR=`dirname $SCRIPT`
 
-jq -cM -L "$BASEDIR/voronoi" -f voronoi/voronoi.jq
+JQ_SCRIPT=voronoi/voronoi.jq
+
+if [[ "$@" =~ --sphere || "$@" =~ -s ]]
+then
+  JQ_SCRIPT=voronoi/voronoi_sphere.jq
+fi
+
+jq -cM -L "$BASEDIR/voronoi" -f ${JQ_SCRIPT}
